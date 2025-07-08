8dabfaff2fec723a97f8a620a5c8f7e50f5c8a57
Kevin <326.osito@gmail.com>
2025-07-07 21:10:48 -0400

    feat: create models for medical_reservation

--- api/models.py
+++ api/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Specialty(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} {self.last_name} - {self.specialty.name}'
+
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f'{self.name} {self.last_name}'
+
+
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    time = models.TimeField()
+
+    class Meta:
+        unique_together = ['doctor', 'date', 'time']
+
+    def __str__(self):
+        return f'{self.date} {self.time} - {self.doctor} with {self.patient}'
