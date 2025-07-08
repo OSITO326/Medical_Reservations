a64b5ed9a34f8cbfc8045a942817710ef897ee9c
Kevin <326.osito@gmail.com>
2025-07-07 21:57:26 -0400

    feat: ModelViewSet for models

--- api/views.py
+++ api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from api.models import Appointment, Doctor, Patient, Specialty
+from api.serializers import (
+    AppointmentSerializer,
+    DoctorSerializer,
+    PatientSerializer,
+    SpecialtySerializer,
+)
+
+
+class SpecialtyViewSet(viewsets.ModelViewSet):
+    queryset = Specialty.objects.all()
+    serializer_class = SpecialtySerializer
+
+
+class DoctorViewSet(viewsets.ModelViewSet):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+
+
+class PatientViewSet(viewsets.ModelViewSet):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+
+class AppointmentViewSet(viewsets.ModelViewSet):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+
+    # def perform_create(self, serializer):
+    #     serializer.save()
+    #
+    # def perform_update(self, serializer):
+    #     serializer.save()
