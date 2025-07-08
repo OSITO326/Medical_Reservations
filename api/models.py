5958d3b5b4ecb8527fd7a2648cdf894a93e6c343
Kevin <326.osito@gmail.com>
2025-07-07 21:48:50 -0400

    add: custom validator and created_at/updated_at in models

--- api/models.py
+++ api/models.py
@@ -1,27 +1,35 @@
 from django.db import models
 
+from api.validators import email_validator, only_letters
+
 
 class Specialty(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, validators=[only_letters])
     description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
 
 class Doctor(models.Model):
-    name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, validators=[only_letters])
+    last_name = models.CharField(max_length=100, validators=[only_letters])
     specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.name} {self.last_name} - {self.specialty.name}'
 
 
 class Patient(models.Model):
-    name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100, validators=[only_letters])
+    last_name = models.CharField(max_length=100, validators=[only_letters])
+    email = models.EmailField(unique=True, validators=[email_validator])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.name} {self.last_name}'
@@ -30,8 +38,11 @@ class Patient(models.Model):
 class Appointment(models.Model):
     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    reason = models.TextField(blank=True, null=True)
     date = models.DateTimeField()
     time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         unique_together = ['doctor', 'date', 'time']
