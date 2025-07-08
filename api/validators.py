d780dd0059ce075720cf3b1e6ea25970b9715d70
Kevin <326.osito@gmail.com>
2025-07-07 21:49:09 -0400

    feat: custom validators

---
+++ api/validators.py
@@ -0,0 +1,12 @@
+from django.core.exceptions import ValidationError
+import re
+
+
+def only_letters(value):
+    if not re.match(r'^[A-Za-z ]+$', value):
+        raise ValidationError('Only letters are allowed.')
+
+
+def email_validator(value):
+    if not re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', value):
+        raise ValidationError('Invalid email format.')
